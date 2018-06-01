53effec2101029193146c96e38457b6ff2009886
Jennifer Hong <jennifer.hong13@gmail.com>
2018-06-01 03:57:08 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
-def display_ranbow(color)
+def display_rainbow("R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet")
   puts "R: red[0], O: orange[1], Y: yellow[2], G: green[3], B: blue[4], I: indigo[5], V: violet[6]"
 end
