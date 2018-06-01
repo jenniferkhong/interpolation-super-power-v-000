0742bcc7c2c226d66a996e7982fe69bcf3f8128c
Jennifer Hong <jennifer.hong13@gmail.com>
2018-06-01 03:58:02 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,5 @@
 def display_rainbow("R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet")
   puts "R: red[0], O: orange[1], Y: yellow[2], G: green[3], B: blue[4], I: indigo[5], V: violet[6]"
 end
+
+display_rainbow(R: red)
